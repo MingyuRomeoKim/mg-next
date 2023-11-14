4c84dcbdc11284b802a3c5ff30f9d7d06f8b8f66
MingyuRomeoKim <rlaalsrb0466@naver.com>
2023-11-14 20:29:52 +0900

    [Next.js 탐험기] 3. Next.js + 티스토리 API 블로그 만들기 - SideBar 구현

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,17 +1,37 @@
 // components/SideBar/SideBar.js
 
+import Link from 'next/link';
 import React from 'react';
 import styles from './SideBar.module.css';
+import categoriesData from '../../data/categories.json'; // JSON 데이터를 import.
 
 const SideBar = () => {
     return (
         <nav className={styles.sideBar}>
-            {/* 여기에 사이드바 내용을 추가하세요. 예를 들어, 메뉴 항목 등 */}
             <ul className={styles.navList}>
-                <li className={styles.navItem}>Home</li>
-                <li className={styles.navItem}>About</li>
-                <li className={styles.navItem}>Services</li>
-                <li className={styles.navItem}>Contact</li>
+                {categoriesData.categories.map((category) => (
+                    <li key={category.id} className={styles.navItem}>
+                        {category.label.includes('#') ? (
+                            <span>{category.label}</span>
+                        ) : (
+                            <Link href={`/${category.name}`}>
+                                {category.label}
+                            </Link>
+                        )}
+                        {/* subCategories가 있을 경우, 중첩된 리스트를 렌더링. */}
+                        {category.subCategories && (
+                            <ul className={styles.subNavList}>
+                                {category.subCategories.map((subCategory) => (
+                                    <li key={subCategory.id} className={styles.subNavItem}>
+                                        <Link href={`/${category.name}/${subCategory.name}`}>
+                                            {subCategory.label}
+                                        </Link>
+                                    </li>
+                                ))}
+                            </ul>
+                        )}
+                    </li>
+                ))}
             </ul>
         </nav>
     );
