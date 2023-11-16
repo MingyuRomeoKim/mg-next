b9aebff1ba2ed58f8d0d024c5e65bd1ca47d271c
MingyuRomeoKim <rlaalsrb0466@naver.com>
2023-11-16 21:20:31 +0900

    [Next.js 탐험기] 5. Next.js + 티스토리 API 블로그 만들기 - 카테고리 페이지 만들기

--- src/app/[categoryName]/page.js
+++ src/app/[categoryName]/page.js
@@ -1,16 +1,26 @@
 'use client'
 
 import React from 'react';
-import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import Link from 'next/link';
+import {usePathname, useRouter, useSearchParams} from 'next/navigation';
+import styles from './[subCategoryName]/page.module.css';
 
-const CategoryPage = (props) => {
+const CategoryPage = () => {
     const pathname = decodeURI(usePathname());
     const params = useSearchParams();
-
-    return(
+    const data = require('../../data/categories/' + params.get('categoryId') + '.json')
+    return (
         <>
-            <div>여기는 {pathname} 카테고리 페이지입니다.</div>
-            <div>여기는 {params.get('categoryId')} categoryId 페이지입니다.</div>
+            <div className={styles.head}>여기는 {pathname.split("/").pop()} 카테고리 페이지입니다.</div>
+            <div className={styles.container}>
+                {Object.entries(data).map(([key, value]) => (
+                    <div className={styles.list}>
+                        <div className={styles.listHead}>
+                            <Link href={value.postUrl} target={'_blank'}>${value.title}</Link>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </>
     );
 };
