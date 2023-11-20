c71bb8814f78f4a438d776e4c2d12da4914ca08c
MingyuRomeoKim <rlaalsrb0466@naver.com>
2023-11-20 15:16:37 +0900

    중복 데이터 방지 코드 삽입 및 요약글 200글자까지 수집 코드 추가

--- src/app/[categoryName]/page.js
+++ src/app/[categoryName]/page.js
@@ -11,22 +11,44 @@ const CategoryPage = () => {
     const [categoryLists, setCategoryLists] = useState(null);
 
     useEffect(() => {
+        // 중복된 categoryList.id를 추적하기 위한 Set
+        const processedIds = new Set();
+
         // 카테고리 JSON 파일을 불러온다.
         fetch(`/data/categories/${params.get('categoryId')}.json`)
             .then(response => response.json())
             .then(async data => {
                 // 각 카테고리에 대한 추가적인 데이터를 불러온다.
                 const updatedCategoryLists = await Promise.all(data.map(async categoryList => {
+                    // 이미 처리된 id는 건너뛴다.
+                    if (processedIds.has(categoryList.id)) {
+                        return null;
+                    }
+                    processedIds.add(categoryList.id);
+
                     const response = await fetch(`/data/posts/${params.get('categoryId')}/${categoryList.id}.json`);
                     const postData = await response.json();
                     // 원문 글에서 데이터 파싱하기
                     const parser = new DOMParser();
                     const htmlDocument = parser.parseFromString(postData.content, "text/html");
-                    const summaryParagraph = htmlDocument.querySelectorAll("p")[0].textContent + "...";
+                    const paragraphs = htmlDocument.querySelectorAll("p");
+
+                    let summaryParagraph = "";
+                    for (let p of paragraphs) {
+                        if (p.textContent.trim().length > 0) {
+                            summaryParagraph = p.textContent + "...";
+                            if (summaryParagraph.length >= 200) {
+                                // 200글자가 넘으면 루프를 중단합니다.
+                                break;
+                            }
+                        }
+                    }
+
                     return {...categoryList, description: summaryParagraph};
                 }));
 
-                setCategoryLists(updatedCategoryLists);
+                // null 값을 제거한 후 categoryLists를 업데이트합니다.
+                setCategoryLists(updatedCategoryLists.filter(item => item !== null));
             })
             .catch(error => console.error('Error loading category lists:', error));
     }, [params]);
---
+++ src/app/about/page.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function About() {
+    return (
+        <>
+            About 화면입니다.
+        </>
+    );
+}
